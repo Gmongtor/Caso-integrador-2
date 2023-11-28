53a612e1726fd4ca896dec42c58b044690262a3c
Gmongtor <gmongtor@myuax.com>
2023-11-28 12:27:06 +0100

    Agregamos la clase variant

--- Ejercicio 1/main.cpp
+++ Ejercicio 1/main.cpp
@@ -3,11 +3,42 @@
 #include <vector>
 #include <map>
 #include "json.hpp"
-
 using namespace std;
 
 class Variant {
 public:
-    typedef Variant (*proc_type)(const Variant&, const Variant&);
+    typedef Variant(*proc_type) ( const std::vector<Variant>& );
+    typedef std::vector<Variant>::const_iterator iter;
+    typedef std::map<std::string, Variant> map;
 
+    variant_type type;
+    std::string val;
+    std::vector<Variant> list;
+    proc_type proc;
+    Entorno* env;
+
+    Variant(variant_type type = Symbol) : type(type) , env(0), proc(0) { }
+    Variant(variant_type type, const std::string& val) : type(type), val(val) , env(0) , proc(0) { }
+    Variant(proc_type proc) : type(Proc), proc(proc) , env(0) { }
+
+    std::string to_string();
+    std::string to_json_string();
+    static Variant from_json_string(std::string json);
+    static Variant parse_json(jsonlib::Json job);  // Asegúrate de que esta es la definición correcta de la función
 };
+
+std::string Variant::to_string() {
+    // Tu implementación aquí
+}
+
+std::string Variant::to_json_string() {
+    // Tu implementación aquí
+}
+
+Variant Variant::from_json_string(std::string sjson) {
+    // Tu implementación aquí
+}
+
+Variant Variant::parse_json(jsonlib::Json job) {
+    // Tu implementación aquí
+}
